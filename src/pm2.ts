c4853d8d76336dc744564875275fc7cf38ecbfd3
Danny Coates <dannycoates@gmail.com>
2020-04-19 09:47:48 -0700

    use inline session / ui tweaks

--- src/pm2.ts
+++ src/pm2.ts
@@ -1,77 +1,95 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 // @ts-ignore
-import { custom as PM2api } from 'pm2'
-import type * as PM2API from 'pm2'
+import { custom as PM2api } from "pm2";
+import type * as PM2API from "pm2";
 
-const inspectRE = /--inspect=(\d+)/
+const inspectRE = /--inspect=(\d+)/;
 
 function matchPort(str: string) {
-  const match = inspectRE.exec(str)
+  const match = inspectRE.exec(str);
   if (match) {
-    return parseInt(match[1], 10)
+    return parseInt(match[1], 10);
   }
-  return null
+  return null;
 }
 
 function findDebugPort(d: PM2API.ProcessDescription) {
-  const x = d as any
-  return matchPort(x.pm2_env.args) || matchPort(x.pm2_env.NODE_OPTIONS)
+  const x = d as any;
+  return matchPort(x.pm2_env.args) || matchPort(x.pm2_env.NODE_OPTIONS);
 }
 
 export class PM2 {
-  pm2api: typeof PM2API
-  
+  pm2api: typeof PM2API;
+
   static async connect() {
-    console.log('connecting')
-    const instance = new PM2api()
+    console.log("connecting");
+    const instance = new PM2api();
     await new Promise((resolve, reject) => {
-      instance.connect((err:Error, meta: any) => {
-        if (err) { return reject(err) }
-        resolve(meta)
-        console.log('connected')
-      })
-    })
-    return new PM2(instance)
+      instance.connect((err: Error, meta: any) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(meta);
+        console.log("connected");
+      });
+    });
+    return new PM2(instance);
   }
 
   private constructor(pm2api: typeof PM2API) {
-    this.pm2api = pm2api
+    this.pm2api = pm2api;
   }
 
   list(): Promise<PM2API.ProcessDescription[]> {
     return new Promise((resolve, reject) => {
       this.pm2api.list((err, list) => {
-        if (err) { return reject(err) }
-        resolve(list)
-      })
-    })
+        if (err) {
+          return reject(err);
+        }
+        resolve(list);
+      });
+    });
   }
 
   describe(process: string | number): Promise<PM2API.ProcessDescription> {
     return new Promise((resolve, reject) => {
       this.pm2api.describe(process, (err, list) => {
-        if (err) { return reject(err) }
-        resolve(list[0])
-      })
-    })
+        if (err) {
+          return reject(err);
+        }
+        resolve(list[0]);
+      });
+    });
   }
 
-
   async pickProcess() {
-    const ps = await this.list()
-    const item = await vscode.window.showQuickPick(
-      ps.map<any>(p => ({
-        label: p.name,
-        description: p.pm2_env?.status,
-        detail: `debugger port: ${findDebugPort(p)}`,
-        debugPort: findDebugPort(p)
-      }))
-      .filter(p => p.description === 'online' && p.debugPort), { canPickMany: false })
+    const ps = await this.list();
+    const picks = ps
+      .map<any>((p) => {
+        // @ts-ignore
+        const description = p.pm2_env.PORT && `port: ${p.pm2_env.PORT}`;
+        return {
+          label: p.name,
+          description,
+          status: p.pm2_env?.status,
+          debugPort: findDebugPort(p),
+          // @ts-ignore
+          port: p.pm2_env.PORT,
+        };
+      })
+      .filter((p) => p.status === "online" && p.debugPort);
+    if (!picks.length) {
+      vscode.window.showErrorMessage("No debuggable PM2 processes are running");
+      return null;
+    }
+    const item = await vscode.window.showQuickPick(picks, {
+      canPickMany: false,
+    });
 
-    return item ? `${item.label}|${item.debugPort}` : null
+    return item ? `${item.label}|${item.debugPort}|${item.port}` : null;
   }
 
   dispose() {
-    this.pm2api.disconnect()
+    this.pm2api.disconnect();
   }
 }
