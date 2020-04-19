cf3497fa014577c25181738f7285881c98831f01
Danny Coates <dannycoates@gmail.com>
2020-04-19 14:26:05 -0700

    added vscode-js-debug support
    https://github.com/microsoft/vscode-js-debug

--- src/extension.ts
+++ src/extension.ts
@@ -8,12 +8,12 @@ let pm2: PM2;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
+  pm2 = await PM2.connect();
   context.subscriptions.push(
     vscode.commands.registerCommand("extension.pickPM2Process", (config) =>
       pm2.pickProcess()
     )
   );
-  pm2 = await PM2.connect();
   context.subscriptions.push(pm2);
   context.subscriptions.push(
     vscode.debug.registerDebugConfigurationProvider(
@@ -28,10 +28,21 @@ export function deactivate() {}
 
 class Pm2NodeDebugConfigurationProvider
   implements vscode.DebugConfigurationProvider {
-  resolveDebugConfiguration(
+  async resolveDebugConfiguration(
     folder: vscode.WorkspaceFolder | undefined,
     cfg: vscode.DebugConfiguration
-  ): vscode.ProviderResult<vscode.DebugConfiguration> {
+  ): Promise<vscode.DebugConfiguration> {
+    if (vscode.workspace.getConfiguration().get("debug.node.useV3", false)) {
+      // ensure it's available and active
+      const x = vscode.extensions.getExtension("ms-vscode.js-debug-nightly");
+      if (x) {
+        if (!x.isActive) {
+          await x.activate();
+        }
+        cfg.__workspaceFolder = "${workspaceFolder}";
+        cfg.useV3 = true;
+      }
+    }
     return cfg;
   }
   resolveDebugConfigurationWithSubstitutedVariables(
@@ -41,7 +52,6 @@ class Pm2NodeDebugConfigurationProvider
     if (!cfg.service) {
       return undefined;
     }
-    const useV3 = false; //vscode.workspace.getConfiguration().get('debug.node.useV3', false)
     const servicePort = cfg.service.split("|");
     let name = `PM2: ${servicePort[0]}`;
     if (servicePort[2] !== "undefined") {
@@ -50,7 +60,8 @@ class Pm2NodeDebugConfigurationProvider
     cfg.name = name;
     cfg.port = parseInt(servicePort[1], 10);
     delete cfg.service;
-    cfg.type = useV3 ? "pwa-node" : "node2";
+    cfg.type = cfg.useV3 ? "pwa-node" : "node2";
+    delete cfg.useV3;
     return cfg;
   }
 }
